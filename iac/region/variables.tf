2b22bac3d8afa957b5f7cc0c6a1323c838c29cae
unknown <elangokannan75@gmail.com>
2022-07-19 08:45:32 +0000

    test1

--- iac/region/variables.tf
+++ iac/region/variables.tf
@@ -63,8 +63,8 @@ variable "matchmaker_vm_storage_account_type" {
 #but increase your quota in your Azure portal for NV12s_v3's and use those below as they have a newer, more 
 #powerful CPU for similar price. NV6 was the default chosen below to avoid quota errors when using this for the first time.
 variable "vmss_size" {
-  default = "Standard_NV6"
-  #default = "Standard_NV12s_v3"
+  #default = "Standard_NV6"
+  default = "Standard_NV12s_v3"
 }
 
 #MSFT created an image in the marketplace that has all the pre-reqs install on Windows 10 for the MM and SS VMs.
